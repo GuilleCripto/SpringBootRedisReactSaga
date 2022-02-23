203594bc550ac913e9e52215445b2824552da176
Guillermo <jgyanez1@gmail.com>
2022-02-23 17:10:17 -0400

    Change comment Initial Server

--- src/main/java/dev/abel/springbootredis/controller/StudentController.java
+++ src/main/java/dev/abel/springbootredis/controller/StudentController.java
@@ -17,7 +17,7 @@ public class StudentController {
 
     @GetMapping("/Inicio")
     public String inicio() {
-        //comentario Inicial 1
+        //comentario Inicial change server
         return  "{var1 : 1, var2:2}";
 
         }
